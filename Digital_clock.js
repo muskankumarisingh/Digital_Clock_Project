aa28682bc778561611598ad1d1d8624e2df078b7
muskankumarisingh <81345123+muskankumarisingh@users.noreply.github.com>
2021-09-20 09:19:53 +0530

    Add files via upload

---
+++ Digital_clock.js
@@ -0,0 +1,32 @@
+function clock(){
+    var hours=document.getElementById("hour");
+    var minutes=document.getElementById("minutes");
+    var seconds=document.getElementById("seconds");
+    var ampm=document.getElementById("ampm");
+    var h=new Date().getHours();
+    var m=new Date().getMinutes();
+    var s=new Date().getSeconds();
+    var am = "AM";
+    if (h>12){
+        h=h-12;
+        var am="PM"
+    }
+    if (h == 0) {
+        hr = 12;
+        am_pm = "AM";
+    }
+    if (h<10){
+        h="0"+h
+    }
+    if (m<10){
+        m="0"+m
+    }
+    if (s<10){
+        s="0"+s
+    }
+hours.innerHTML=h;
+minutes.innerHTML=m;
+seconds.innerHTML=s;
+ampm.innerHTML = am;
+}
+var interval=setInterval(clock,1000);
